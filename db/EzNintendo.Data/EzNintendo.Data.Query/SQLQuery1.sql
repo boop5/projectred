69aa6ddb855bd6989c3c4b97676bc9269490d1f3
Lucas Schad <schad.lucas@gmail.com>
2021-06-06 01:20:15 +0200

    Renamed Project & removed old files

--- db/EzNintendo.Data/EzNintendo.Data.Query/SQLQuery1.sql
+++
@@ -1,4 +0,0 @@
-SELECT [t].[Id], [t].[Country], [t].[Created], [t].[Discount], [t].[GameId], [t].[Price], [t0].[Id], [t0].[Country], [t0].[Created], [t0].[Discount], [t0].[GameId], [t0].[Price]
-FROM [Nintendo].[Trend] AS [t]
-INNER JOIN [Nintendo].[Trend] AS [t0] ON ([t].[GameId] = [t0].[GameId]) AND ((([t].[Country] = [t0].[Country]) AND ([t].[Country] IS NOT NULL AND [t0].[Country] IS NOT NULL)) OR ([t].[Country] IS NULL AND [t0].[Country] IS NULL))
-WHERE ([t].[Created] > [t0].[Created]) 
